3073516f46d046c07b4b3373071dc3925c17a12f
naoki kodama <naoki.koda@gmail.com>
2011-11-01 01:52:24 +0900

    曲、チューニング、アルバムとの関連のシードを追加。これはいける！

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: UTF-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -7,12 +8,26 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 ["Kotaro Oshio"].each do |alb|
-  Album.find_or_create_by_title(alb)
+  al = Album.find_or_create_by_title(alb)
+  [
+    ["光のつばさ", "CGDGCD"],
+    ["彩音",  "CGDGCD"],
+  ].each do |t|
+    tune   = Tune.find_or_create_by_title(t[0])
+    tuning = Tuning.find_or_create_by_name(t[1])
+    tuning.capo ||= t[2] != nil ? t[2] : 0
+    tuning.tunes << tune
+    tuning.save
+
+    recording = Recording.new
+    tune.recordings << recording
+    al.recordings << recording
+    tune.save
+  end
+  al.save
 end
 
 =begin
-光のつばさ  CGDGCD
-彩音  CGDGCD
 第三の男  DGDGBE
 禁じられた遊び  Standard
 アイルランドの風  AAEGAD

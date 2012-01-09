50c6de3ce885d2fac635b724cae39d9e04cb12d4
Kazuya Numata <nu@nu-chon.org>
2012-01-09 17:19:59 +0900

    Change the way to create of the comment in CommentsController#create.
    The way to save a comment has called three SQLs.
      1. insert a record ( both user_id and tune_id are null )
      2. update the record ( update with user_id, still tune_id is null )
      3. update the record ( update with tune_id. finally the record is sufficient status. )
    
      INSERT INTO "comments" ("created_at", "text", "tune_id", "updated_at", "user_id") VALUES (?, ?, ?, ?, ?)  [["created_at", Mon, 09 Jan 2012 16:40:52 JST +09:00], ["text", "hogehoge"], ["tune_id", nil], ["updated_at", Mon, 09 Jan 2012 16:40:52 JST +09:00], ["user_id", nil]]
      UPDATE "comments" SET "user_id" = 1, "updated_at" = '2012-01-09 16:40:52.143598' WHERE "comments"."id" = 1
      UPDATE "comments" SET "tune_id" = 1, "updated_at" = '2012-01-09 16:40:52.156357' WHERE "comments"."id" = 1
    
    The first Comment.create is redundant insert and the record is in insufficient status
    until the second save call because tune_id and user_id are not stored.
    
    This commit call only one SQL to store a record with the sufficient record status.
    
    INSERT INTO "comments" ("created_at", "text", "tune_id", "updated_at", "user_id") VALUES (?, ?, ?, ?, ?)  [["created_at", Mon, 09 Jan 2012 17:05:42 JST +09:00], ["text", "eeeeeee"], ["tune_id", 2], ["updated_at", Mon, 09 Jan 2012 17:05:42 JST +09:00], ["user_id", 1]]
    
    Additionally, Now the save! is called instead of save (no !) because save status has not been checked and if it failed, the user should notice the failure.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
 class CommentsController < ApplicationController
   def create
     user = User.find(params[:user_id])
     tune = Tune.find(params[:tune_id])
 
-    comment = Comment.create(params[:comment])
-    user.comments << comment
-    tune.comments << comment
-    comment.save
+    comment = user.comments.build(params[:comment])
+    comment.tune = tune
+    comment.save!
+    
     redirect_to user_tune_path(user,tune)
   end
 

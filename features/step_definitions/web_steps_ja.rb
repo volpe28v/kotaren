5639ec89044f6a15f298c4d25126f5db01eb0fbc
naoki kodama <naoki.koda@gmail.com>
2012-01-26 18:53:38 +0900

    仕様化テストを追加：新規登録でログインする

--- features/step_definitions/web_steps_ja.rb
+++ features/step_definitions/web_steps_ja.rb
@@ -38,3 +38,32 @@ end
   page.should have_content("This is You!")
 end
 
+もし /^　  新規登録を選択する$/ do
+  click_on("新規登録して使ってみる")
+end
+
+ならば /^　新規登録画面を表示すること$/ do
+  page.should have_content("新規登録")
+  page.should have_content("ログイン後にも変更可能です。")
+end
+
+前提 /^　　新規登録画面を表示している$/ do
+  visit('/')
+  click_on("新規登録して使ってみる")
+end
+
+もし /^　  ユーザ "([^"]*)" を登録する$/ do |user_name|
+  fill_in("user_name", :with => user_name) 
+  fill_in("user_email", :with => "#{user_name}@kotaren.com")
+  fill_in("user_password", :with => "#{user_name}#{user_name}")
+  fill_in("user_password_confirmation", :with => "#{user_name}#{user_name}")
+
+  click_on("登録")
+end
+
+ならば /^　"([^"]*)" 楽曲リストを表示すること$/ do |user_name|
+  page.should have_content(user_name)
+  page.should have_content("押尾コータロー楽曲リスト")
+end
+
+

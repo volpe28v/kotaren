91252ed2eaed70638aaf0636fbae002a5b078788
naoki kodama <naoki.koda@gmail.com>
2012-01-26 15:06:56 +0900

    仕様化テストを追加：サンプルアカウントでログインする

--- features/step_definitions/web_steps_ja.rb
+++ features/step_definitions/web_steps_ja.rb
@@ -8,4 +8,33 @@ end
   page.should have_content("コタれんって")
 end
 
+前提 /^　　トップページを表示している$/ do
+  visit('/')
+end
+
+前提 /^　　サンプルアカウントが存在する$/ do
+  user = User.create(:name => "サンプルアカウント",
+              :email => "sample@sample.kotaren",
+              :password => "sample",
+              :password_confirmation => "sample")
+  user.should_not be_nil
+end
+
+もし /^　　サンプルアカウントでログインする$/ do
+  #TODO: 本当は「サンプルアカウントでログイン」リンクを
+  #      クリックしたいけど、link 内の onclick イベント
+  #      が起動してくれないので、直接隠しボタンをクリッ
+  #      クしている。
+  #click_link "サンプルアカウントでログイン"
+  find("#sample-login").click
+end
+
+ならば /^　サンプルアカウントの楽曲リストを表示すること$/ do
+  page.should have_content("サンプルアカウント")
+  page.should have_content("押尾コータロー楽曲リスト")
+end
+
+ならば /^　カレントユーザがサンプルアカウントであること$/ do
+  page.should have_content("This is You!")
+end
 

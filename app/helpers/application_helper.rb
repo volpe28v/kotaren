219765f171ab16fe5e1216864e06e47dfd4fffcc
naoki kodama <naoki.koda@gmail.com>
2012-04-09 22:32:04 +0900

    Add a facebook like button on top page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def fb_like(url)
+    raw ('<div class="fb-like" data-href="' + url + '" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="arial"></div>')
+  end
+
 end

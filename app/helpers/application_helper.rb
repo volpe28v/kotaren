c6b1e733937cc25f087345edb61f165ffc6e7416
naoki kodama <naoki.koda@gmail.com>
2012-04-15 23:31:49 +0900

    Add a "弾いたよ" button and display update logic of progress.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
     if time < 3.month.ago
       return raw('<span class="label label-important">last played at ' + time.strftime("%Y-%m-%d") + '</span>')
     elsif time < 1.month.ago
-      return raw('<span class="label label-warning">last played at ' + time.strftime("%Y-%m-%d") + '</span>')
+      return raw('<span class="label label-success">last played at ' + time.strftime("%Y-%m-%d") + '</span>')
     end
 
     return raw('<span class="label label-info">last played at ' + time.strftime("%Y-%m-%d") + '</span>')

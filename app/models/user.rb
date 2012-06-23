9f717ffc18d444be95f9ccc80c652ff17b0d0289
naoki kodama <naoki.koda@gmail.com>
2012-06-23 16:39:39 +0900

    Change using find_by_email on find_for_facebook_oauth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,7 +50,7 @@ class User < ActiveRecord::Base
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
 #    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    user = User.first(:email => auth.info.email)
+    user = User.find_by_email(auth.info.email)
 
     unless user
       user = User.create(name:auth.extra.raw_info.name,

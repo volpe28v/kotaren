4d19478aaabd70e38ce18e2a3f5c4e60dda4e561
naoki kodama <naoki.koda@gmail.com>
2012-06-23 16:35:25 +0900

    Change find_for_facebook_oauth find logic with email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,11 +49,13 @@ class User < ActiveRecord::Base
   end
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+#    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    user = User.first(:email => auth.info.email)
+
     unless user
       user = User.create(name:auth.extra.raw_info.name,
-                         provider:auth.provider,
-                         uid:auth.uid,
+#                         provider:auth.provider,
+#                         uid:auth.uid,
                          email:auth.info.email,
                          password:Devise.friendly_token[0,20]
                          )

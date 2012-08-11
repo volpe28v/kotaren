5f9e581e625a4c3a4683d13223e1763f723de8cb
naoki kodama <naoki.koda@gmail.com>
2012-08-11 15:09:48 +0900

    Add progress average to album thumbnail.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,12 @@
 class Album < ActiveRecord::Base
   has_many :recordings
   has_many :tunes , :through => :recordings
+
+  def progress_average(user)
+    progress_sum = self.tunes.inject(0){|sum,t|
+      sum += t.progress_val(user)
+    }
+
+    return progress_sum / self.tunes.size
+  end
 end

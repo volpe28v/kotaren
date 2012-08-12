dc05fe01556c29daa40abab67e75112765a58e51
naoki kodama <naoki.koda@gmail.com>
2012-08-13 01:18:55 +0900

    Remove unuse code.

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -3,16 +3,6 @@ class RankingController < ApplicationController
     @tune_ranking = Tune.get_tune_ranking
   end
 
-  def players
-    @user = current_user
-    @tune = Tune.find(params[:tune_id])
-    @progress_val = @tune.progress_val(@user)
-    @comment = Comment.new
-    @comments = @user.comments.by_tune(@tune)
-    @other_comments = Comment.other_by(@user).by_tune(@tune)
-    @other_member_count = Progress.includes(:tune).where("tunes.id = ? and percent > 0", @tune.id).count - ( @progress_val > 0 ? 1 : 0 )
-  end
-
   def latest_played
     @progresses = Progress.order("updated_at DESC").limit(30)
   end

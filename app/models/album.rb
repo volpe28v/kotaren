e398c0435ee02736a5830f45d4a3cea419f371d3
SHIMADA Koji <koji.shimada@enishi-tech.com>
2012-09-09 13:54:21 +0900

    Extract #progress_sum_by_user

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,12 +3,14 @@ class Album < ActiveRecord::Base
   has_many :tunes , :through => :recordings
 
   def progress_average(user)
-    progress_sum = progresses_of_tunes.by_user(user).sum(:percent)
-
-    return progress_sum / self.tunes.size
+    return progress_sum_by_user(user) / self.tunes.size
   end
 
   private
+  def progress_sum_by_user(user)
+    progresses_of_tunes.by_user(user).sum(:percent)
+  end
+
   def progresses_of_tunes
     Progress.of_tunes(self.tunes)
   end

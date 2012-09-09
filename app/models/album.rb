330687a556d3a7bf574fcd4dce376ca99e733fc9
SHIMADA Koji <koji.shimada@enishi-tech.com>
2012-09-09 13:56:01 +0900

    Extract #number_of_tunes

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,10 +3,14 @@ class Album < ActiveRecord::Base
   has_many :tunes , :through => :recordings
 
   def progress_average(user)
-    return progress_sum_by_user(user) / self.tunes.size
+    return progress_sum_by_user(user) / number_of_tunes
   end
 
   private
+  def number_of_tunes
+    self.tunes.size
+  end
+
   def progress_sum_by_user(user)
     progresses_of_tunes.by_user(user).sum(:percent)
   end

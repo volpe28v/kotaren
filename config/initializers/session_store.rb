d6ae8fb25545b68f8d811693beeff77fc4f28dae
naoki kodama <naoki.koda@gmail.com>
2012-11-05 00:09:24 +0900

    Change session expire to 3 month.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Kotaren::Application.config.session_store :cookie_store, key: '_kotaren_session'
+Kotaren::Application.config.session_store :cookie_store,
+  key: '_kotaren_session',
+  expire_after: 3.months
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

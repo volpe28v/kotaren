77e3008cdd8b5cf8b258efd3ed350c781705e765
naoki kodama <naoki.koda@gmail.com>
2012-12-08 15:08:06 +0900

    Add delete reply link (only)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -38,7 +38,7 @@ class CommentsController < ApplicationController
 
   def load_comment_list
     @albums = Album.scoped.order("id ASC")
-    @comments = Comment.scoped.order("updated_at DESC").limit(50)
+    @comments = Comment.scoped.order("updated_at DESC").limit(30)
 
     lists = render_to_string :partial => 'comment_list_smart_phone'
     render :json => { lists: lists },

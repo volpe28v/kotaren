910137b14af33b6e720459b0b2f6ab5665e7fc6d
naoki kodama <naoki.koda@gmail.com>
2013-02-03 20:25:21 +0900

    Add config of mail for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,17 @@ Kotaren::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'gmail.com',
+    :user_name => ENV['MAIL_ADDRESS'],
+    :password => ENV['MAIL_PASSWORD'],
+    :authentication => 'plain',
+    :enable_starttls_auto => true,
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 

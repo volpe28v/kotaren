8dc5f42af04883b95e96a1582716a108f7c5ad65
naoki kodama <naoki.koda@gmail.com>
2013-04-27 17:56:28 +0900

    Delete un managed specs.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe CommentsController do
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
 end

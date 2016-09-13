11dc5caafde0f4d23ed96c9f1a2326471e2e7820
naoki kodama <naoki.koda@gmail.com>
2016-09-13 23:55:41 +0900

    onsen-ui を最新にアップグレードし、 knockout 用のコードを修正。チェックボックスも出た

--- app/assets/javascripts/kotaren_smart_phone.js
+++ app/assets/javascripts/kotaren_smart_phone.js
@@ -1,59 +1,56 @@
-  // Rewrite Navigator behavior to connect the new page with the viewModel
-  window.OnsNavigatorElement.rewritables.link = function(element, target, options, callback) {
-    if (!options.viewModel) {
-      options.viewModel = new ListViewModel();
+ons.ready(function(){
+  document.querySelector('ons-navigator').pushPage('list.html');
+});
+
+window.ons.NavigatorElement.rewritables.link = function(element, target, options, callback) {
+  if (!options.viewModel) {
+    options.viewModel = new ListViewModel();
+  }
+  ko.applyBindings(options.viewModel, target);
+  callback(target);
+};
+
+// Item model
+function ListItem(content) {
+  var self = this;
+  self.content = content;
+}
+
+
+// Details page view model
+function DetailsViewModel(item) {
+  var self = this;
+  self.item = item;
+}
+
+// List page view model
+function ListViewModel() {
+  var self = this;
+
+  self.items = ko.observableArray([]);
+  $.ajax({
+    type: "GET",
+    cache: false,
+    url: "/api/tunes",
+    success: function (data) {
+      console.log(data);
+      self.items(data);
     }
-    ko.applyBindings(options.viewModel, target);
-    callback(target);
+  });
+
+  self.nextCarouselItem = function(index) {
+    document.querySelectorAll('ons-carousel')[index()].next();
+  }
+
+  self.addItem = function() {
+    self.items.push(new ListItem('Item ' + self.items().length));
   };
 
-  // Item model
-  function ListItem(content) {
-    var self = this;
-    self.content = content;
+  self.removeItem = function() {
+    self.items.remove(this);
   }
 
-
-  // Details page view model
-  function DetailsViewModel(item) {
-    var self = this;
-    self.item = item;
+  self.detailsItem = function() {
+    document.querySelector('ons-navigator').pushPage('details.html', {viewModel: new DetailsViewModel(this)});
   }
-
-  // List page view model
-  function ListViewModel() {
-    var self = this;
-
-    self.items = ko.observableArray([]);
-    $.ajax({
-      type: "GET",
-      cache: false,
-      url: "/api/tunes",
-      success: function (data) {
-        console.log(data);
-        self.items(data);
-      }
-    });
-
-    /*
-    for(var i = 0; i < 10; i++) {
-      self.items.push(new ListItem("Item " + i));
-    }
-    */
-
-    self.nextCarouselItem = function(index) {
-      document.querySelectorAll('ons-carousel')[index()].next();
-    }
-
-    self.addItem = function() {
-      self.items.push(new ListItem('Item ' + self.items().length));
-    };
-
-    self.removeItem = function() {
-      self.items.remove(this);
-    }
-
-    self.detailsItem = function() {
-      document.querySelector('ons-navigator').pushPage('details.html', {viewModel: new DetailsViewModel(this)});
-    }
-  }
+}

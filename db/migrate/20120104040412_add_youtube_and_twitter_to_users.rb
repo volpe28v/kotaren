4f72c855bb9ed4032897ede5c7c19cd3cb3dab1b
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-12-18 18:26:55 +0900

    Devise 1.x の DSL で書かれた migration を書き直しました

--- db/migrate/20120104040412_add_youtube_and_twitter_to_users.rb
+++ db/migrate/20120104040412_add_youtube_and_twitter_to_users.rb
@@ -1,6 +1,6 @@
 class AddYoutubeAndTwitterToUsers < ActiveRecord::Migration
   def change
-    add_column :users ,:youtube_name, :string 
-    add_column :users ,:twitter_name, :string 
+    add_column :users ,:youtube_name, :string
+    add_column :users ,:twitter_name, :string
   end
 end
